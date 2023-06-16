4ca0f30f3aaeec43c276744e0b86e2825e1cd9c8
Sharath083 <sharath.r@marketsimplified.com>
2023-06-16 12:22:20 +0530

    new

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,39 @@
 
-fun main(args: Array<String>) {
-    var d=Methods()
-    d.addMovies()
-    d.search("RRR")
-    d.filter(4.2)
+fun main() {
+    val d=Methods()
+//    d.addMovies()
+//    d.search("RRR")
+//    d.filter(4.2)
+
+//    var repeat:String
+
+    do{
+        println("1-->Add New Movie  2--> Search 3--> searchByRating 4-->View Movie List 5--> Remove Movie ")
+        when(readln().toInt()){
+            1 -> d.addMovies()
+
+            2 ->{
+                println("Enter Search")
+//                val input= readln()
+                d.search(readln())
+            }
+
+            3 ->{
+                println("Enter Rating")
+                d.filter(readln().toDouble())
+            }
+            4-> d.viewMovies()
+            5->{
+                println("Enter movie Name")
+                d.removeMovie(readln())
+            }
+            else->
+                println("Enter valid Input")
+
+
+        }
+        println("y-->continue to do other operation  n--> Exit")
+//        repeat=reader.nextLine()
+    }while (readln().equals("y",true))
 
 }
--- src/main/kotlin/Methods.kt
+++ src/main/kotlin/Methods.kt
@@ -1,4 +1,6 @@
-data class Details(val director:String,val title:String,val year:Int,val rating:Double,val language:String)
+import java.util.*
+
+data class Details(val director:String, val title:String, val year:Int, val rating:Double, val language:String)
 //Create a simplified version of IMDB. Store movies as a mutableList locally. Add attributes such as name, title, year, rating, language.
 //
 //Write a function to search in the mutableList. This function should search all attributes and return all possible results.
@@ -6,17 +8,39 @@ data class Details(val director:String,val title:String,val year:Int,val rating:
 //Write another method which accept a rating as input and return all movies with rating above than the given input.
 class Methods {
     private var movieList=  mutableListOf<Details>()
-    fun addMovies(){
+    private val reader = Scanner(System.`in`)
+    init {
         movieList.add(Details("SSR","RRR",2022,4.6,"Telugu"))
         movieList.add(Details("Rusove","Avengers",2019,4.8,"English"))
         movieList.add(Details("Snyder","SuperMan",2013,4.2,"Telugu"))
         movieList.add(Details("James","Avatar",2010,4.9,"English"))
         movieList.add(Details("Chris","Interstellar",2014,4.6,"English"))
+    }
+
+    fun addMovies(){
+
+//        var again:Int
+        do{
+            println("Enter Director name")
+            val director= readln()
+            println("Enter Movie name")
+            val movie= readln()
+            println("Enter Year")
+            val year=reader.nextInt()
+            println("Enter Rating")
+            val rating=reader.nextDouble()
+            reader.nextLine()
+            println("Enter Language")
+            val language=readln()
+            movieList.add(Details(director,movie,year,rating, language))
+            println("y-->Add again  n--> Exit")
+//        repeat=reader.nextLine()
+        }while (readln().equals("y",true))
 
     }
     fun search(input:String){
         val filterList: List<Details> = movieList.filter {
-            it.director == input || it.language == input || it.title == input || it.year.toString()==input
+            it.director.contains(input,true) || it.language.contains(input,true) || it.title.contains(input,true)  || it.year.toString().contains(input,true)
         }
         println(filterList)
     }
@@ -25,6 +49,14 @@ class Methods {
             it.rating > inputRating
         }
         println(ratingList)
+    }
+    fun  viewMovies(){
+        println(movieList)
+    }
+    fun removeMovie(input: String){
+
+        println( "Status : ${movieList.removeIf { it.title.equals(input,true) }}")
+
     }
 
 }
